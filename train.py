5a18fc2e5132030a4eaa8a9eaa85e3ded9ac4d16
Yuji Tanaka <57528969+ujtk6014@users.noreply.github.com>
2021-01-25 14:15:18 +0900

    CHANGE:

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from network import DDPGAgent
 from utils import *
 
 
-def train(batch_size=128, critic_lr=1e-3, actor_lr=1e-4, max_episodes=1000, max_steps=300, gamma=0.99, tau=1e-3,
+def train(batch_size=128, critic_lr=1e-3, actor_lr=1e-4, max_episodes=10000, max_steps=300, gamma=0.99, tau=1e-3,
           buffer_maxlen=100000):
     # simulation of the agent solving the spacecraft attitude control problem
     env = make("SatelliteContinuous")

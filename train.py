266288c9123c9d7a6e561be4297139a78b42f3c4
Yuji Tanaka <57528969+ujtk6014@users.noreply.github.com>
2021-02-01 22:05:55 +0900

    UPDATE: change agent to trained one

--- train.py
+++ train.py
@@ -26,9 +26,9 @@ def train(batch_size=128, critic_lr=1e-3, actor_lr=1e-4, max_episodes=10000, max
     critic_lr = critic_lr
     actor_lr = actor_lr
 
-    agent = TD3Agent(env, gamma, tau, buffer_maxlen, critic_lr, actor_lr, True, max_episodes * max_steps)
+    # agent = TD3Agent(env, gamma, tau, buffer_maxlen, critic_lr, actor_lr, True, max_episodes * max_steps)
     curr_dir = os.path.abspath(os.getcwd())
-    # agent = torch.load(curr_dir + "/models/spacecraft_control_ddpg.pkl")
+    agent = torch.load(curr_dir + "/models/spacecraft_control_ddpg.pkl")
     # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     episode_rewards = mini_batch_train(env, agent, max_episodes, max_steps, batch_size)
 
